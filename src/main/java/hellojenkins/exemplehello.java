6da521f4a10a38588fd10065824df994cfc7316c
aminespankes <48100868+aminespankes@users.noreply.github.com>
2020-04-18 15:39:26 +0100

    Update exemplehello.java

--- src/main/java/hellojenkins/exemplehello.java
+++ src/main/java/hellojenkins/exemplehello.java
@@ -3,7 +3,7 @@ package hellojenkins;
 public class exemplehello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello,   world!");
 	}
 
 }
